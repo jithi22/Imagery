ec66f65c9947927e79abafa8a55ab3a864879fd8
jithi22 <jithinprakash1026@gmail.com>
2022-04-03 21:39:33 +0530

    Imagery1.0

--- printer.py
+++ printer.py
@@ -1,3 +1,20 @@
-fhandle = open(r"store.txt")
-for line in fhandle:
-    print(line)
+dark ='*'
+light ='.'
+
+choice = input("Do you Want custom Symbol (press 1)\n Default Symbols * & . in Use (press any key): ")
+if choice == 1:
+ dark =  input('Enter the symbol to use for dark color :')
+ light =input('Enter the symbol yo use for light color :')
+fhandle = open(r"binarized.txt")
+
+for line in  fhandle:
+    print('\n')
+    for ch in line:
+     if ch == ' ' or ch == '\n':continue   
+     if int(ch) == 1:
+        print(dark,end="")
+     else:
+        print(light,end="") 
+fhandle.close()
+    
+    
--- run.py
+++ run.py
@@ -3,17 +3,18 @@ def clean():
     os.remove('bsImage.jpg')
     os.remove('resized_image.jpg')
     os.remove('binarized.txt')
-    os.remove('store.txt')
-    print("Starting Printer ..\n\n")
+    
 
 try:
     clean()
+    print("Starting Printer\n\n")
+
 except:
     print("Starting Printer\n\n")
+
 import image
 import resize
 import binary
-import binary2
 import printer
 clean()
 
